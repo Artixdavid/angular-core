710482f5b6205750a76e75781d1cfa07bd0f4362
David Leones <davidleones@MacBook-Pro-Hola.local>
2019-02-21 17:16:25 -0300

    components users

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsRoutingModule } from './components-routing.module';
 import { ComponentsComponent } from './components.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		ComponentsRoutingModule
 	],
-	declarations: [ComponentsComponent]
+	declarations: [ComponentsComponent, UserComponent]
 })
 export class ComponentsModule { }
