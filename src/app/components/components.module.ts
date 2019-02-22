938a16b1a36d096b5a27c61479c4cd963c9a2986
David Leones <davidleones@MacBook-Pro-Hola.local>
2019-02-22 16:15:58 -0300

    grid

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,8 @@ import { ComponentsComponent } from './components.component';
 import { MenuModule } from 'primeng/menu';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
 	imports: [
@@ -13,6 +15,6 @@ import { CommonModule } from '@angular/common';
 		FormsModule,
 		ReactiveFormsModule,
 	],
-	declarations: [ComponentsComponent]
+	declarations: [ComponentsComponent, SidebarComponent, TopbarComponent]
 })
 export class ComponentsModule { }
