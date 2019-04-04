71bec8a4e9fd0dc7ec2bce99713ba965ba8df2f3
David <franklinleones@gmail.com>
2019-04-04 13:23:14 -0300

    package-lock

--- src/app/service/api/base.service.ts
+++ src/app/service/api/base.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Base } from '../models/dto/Base';
 import { BaseMetadata } from '../models/dto/BaseMetadata';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
 	providedIn: 'root'
